1a983cdde63a2d29555459e6b0f122e41f18099a
shounakray0710 <85279032+shounakray0710@users.noreply.github.com>
2021-08-01 13:07:56 +0530

    Add files via upload

---
+++ link.js
@@ -0,0 +1,14 @@
+class Link{
+    constructor(bodyA,bodyB)
+    {
+      var lastlink = bodyA.body.bodies.length-2;
+     this.link = Constraint.create(
+        {
+          bodyA:bodyA.body.bodies[lastlink],  
+          bodyB:bodyB,
+          length:-10,
+          stiffness:0.01
+        });
+        World.add(engine.world,this.link);
+    } 
+}
